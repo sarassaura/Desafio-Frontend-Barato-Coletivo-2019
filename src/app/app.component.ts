affd4a0c33a9ee64786f239521fa414f333bc530
sarassaura <sarahyukinonakada@gmail.com>
2024-06-24 18:20:59 -0300

    feat: card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CardComponent],
 })
 export class AppComponent {
   title = 'barato-coletivo';
